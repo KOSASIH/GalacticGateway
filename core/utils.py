a1e1ca87af919f1ddb7c747d111457ed6ecf0450
KOSASIH <kosasihg88@gmail.com>
2024-06-24 20:52:50 +0700

    Create utils.py

---
+++ core/utils.py
@@ -0,0 +1,21 @@
+import random
+
+class Utils:
+    def __init__(self):
+        pass
+
+    def generate_uuid(self) -> str:
+        # Generate a UUID
+        pass
+
+    def get_current_time(self) -> datetime.datetime:
+        # Return the current time
+        pass
+
+    def generate_random_string(self, length: int) -> str:
+        # Generate a random string
+        pass
+
+# Example usage:
+# utils = Utils()
+# uuid = utils.generate_uuid()
